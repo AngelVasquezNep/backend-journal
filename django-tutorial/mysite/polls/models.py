6d7dd420ffd28798c196a7993d83d356ea58521f
Angel Vasquez <angel.vasquez.unam@gmail.com>
2021-11-17 22:25:32 -0600

    Polls: Add Question and Choice model

--- django-tutorial/mysite/polls/models.py
+++ django-tutorial/mysite/polls/models.py
@@ -1,3 +1,33 @@
-from django.db import models
+import datetime
 
-# Create your models here.
+from django.db import models
+from django.utils import timezone
+
+
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
+    def __str__(self) -> str:
+        return f'{self.question_text} ({self.pk})'
+
+    def was_published_recently(self) -> bool:
+        """Note: It works over a Question instance.
+
+        q = Question.objects.first()
+        q.was_published_recently()
+        """
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(
+        'polls.Question',
+        related_name='choices',
+        on_delete=models.CASCADE
+    )
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+
+    def __str__(self) -> str:
+        return f'{self.choice_text} ({self.pk})'
