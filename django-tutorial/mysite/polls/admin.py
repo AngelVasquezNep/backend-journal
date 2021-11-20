d9e2158984544e3055efd23a6ddebc4d3238000f
Angel Vasquez <angel.vasquez.unam@gmail.com>
2021-11-19 21:28:10 -0600

    Creating some models

--- django-tutorial/mysite/polls/admin.py
+++ django-tutorial/mysite/polls/admin.py
@@ -1,5 +1,15 @@
 from django.contrib import admin
 
-from .models import Question
+from .models import (
+    Question,
+    Pizza,
+    Topping,
+    Manufacturer,
+    Car,
+    Person,
+    Group,
+    Membership,
+)
 
-admin.site.register(Question)
+admin.site.register((Question, Pizza, Topping, Manufacturer,
+                    Car, Person, Group, Membership))
--- django-tutorial/mysite/polls/models.py
+++ django-tutorial/mysite/polls/models.py
@@ -21,6 +21,8 @@ class Question(models.Model):
 
 
 class Choice(models.Model):
+    # Choice.objects.first().question -> Me dará sólo 1 registro
+    # Pero Question.objects.first().choices -> Me dará un query set
     question = models.ForeignKey(
         'polls.Question',
         related_name='choices',
@@ -31,3 +33,114 @@ class Choice(models.Model):
 
     def __str__(self) -> str:
         return f'{self.choice_text} ({self.pk})'
+
+
+class Person(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return f'({self.pk}) {self.first_name} {self.last_name}'
+
+
+class Musician(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    instrument = models.CharField(max_length=100)
+
+
+class Album(models.Model):
+    artist = models.ForeignKey(
+        'polls.Musician',
+        on_delete=models.CASCADE,
+        related_name="albums"
+    )
+    name = models.CharField(max_length=100)
+    release_date = models.DateField()
+    num_stars = models.IntegerField()
+
+
+class Manufacturer(models.Model):
+    """Manufacter company
+    """
+    name = models.CharField(max_length=100)
+
+    def __str__(self) -> str:
+        return f'({self.pk}) {self.name}'
+
+
+class Car(models.Model):
+    model = models.CharField(max_length=100)
+    manufacturer = models.ForeignKey(
+        Manufacturer,
+        related_name="cars",
+        on_delete=models.CASCADE
+    )
+
+    def __str__(self) -> str:
+        return f'({self.pk}) {self.model}'
+
+
+Toppings = (
+    ('PEPPERONI', 'PEPPERONI'),
+    ('MOZZARELLA', 'MOZZARELLA'),
+    ('BACON', 'BACON'),
+    ('ONION', 'ONION'),
+    ('CAPSICUM', 'CAPSICUM'),
+    ('MUSHROOM', 'MUSHROOM'),
+    ('OLIVES', 'OLIVES'),
+)
+
+
+Pizzas = (
+    ("PEPPERONI", "PEPPERONI"),
+    ("SUPREME", "SUPREME"),
+    ("HAWAIIAN", "HAWAIIAN"),
+    ("MARGHERITA", "MARGHERITA"),
+    ("CHEESE", "CHEESE"),
+)
+
+
+class Topping(models.Model):
+    name = models.CharField(max_length=100, choices=Toppings)
+
+    def __str__(self) -> str:
+        return f'({self.pk}) {self.name}'
+
+
+class Pizza(models.Model):
+    name = models.CharField(max_length=100, choices=Pizzas)
+    toppings = models.ManyToManyField(
+        'polls.Topping',
+        related_name="pizzas"
+    )
+
+    def __str__(self) -> str:
+        return f'({self.pk}) {self.name}'
+
+
+class Group(models.Model):
+    name = models.CharField(max_length=128)
+    members = models.ManyToManyField(
+        'polls.Person',
+        related_name="groups",
+        through='Membership',
+    )
+
+    def __str__(self):
+        return f'({self.pk}) {self.name}'
+
+
+class Membership(models.Model):
+    person = models.ForeignKey(
+        'polls.Person',
+        related_name='memberships',
+        on_delete=models.CASCADE,
+    )
+    group = models.ForeignKey(
+        'polls.Group',
+        related_name='memberships',
+        on_delete=models.CASCADE
+    )
+    date_joined = models.DateField()
+    invite_reason = models.CharField(max_length=64)
