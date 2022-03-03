141a3ce691c013159bd81c94f462c900f41f6a93
Angel Vasquez <angel.vasquez.unam@gmail.com>
2022-03-02 19:35:09 -0600

    Simple django tests: How to update many to many relationship

--- simple_django_tests/simple_app/library/views.py
+++ simple_django_tests/simple_app/library/views.py
@@ -1,25 +1,52 @@
 from rest_framework import serializers, viewsets, permissions
+from rest_framework.response import Response
 from library.models import Author, Book
 
 
+class BookAuthorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Author
+        fields = ('id', 'name',)
+
+
 class BookSerializer(serializers.ModelSerializer):
+    authors = BookAuthorSerializer(many=True)
+
     class Meta:
         model = Book
         fields = ('id', 'title', 'year', 'authors')
 
+
+class AuthorBookSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Book
+        fields = ('id', 'title', 'year',)
+
+
 class AuthorSerializer(serializers.ModelSerializer):
+    books = AuthorBookSerializer(many=True, read_only=True)
+
     class Meta:
         model = Author
         fields = ('id', 'name', 'books',)
 
 
+
 class AuthorViewSet(viewsets.ModelViewSet):
     queryset = Author.objects.all()
     serializer_class = AuthorSerializer
-    permission_classes = (permissions.IsAuthenticated,)
 
 
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-    permission_classes = (permissions.IsAuthenticated,)
+
+    def update(self, request, *args, **kwargs):
+        partial = kwargs.pop('partial', False)
+        book = self.get_object()
+        authors = request.data.pop('authors')
+        serializer = self.get_serializer(book, data=request.data, partial=partial)
+        serializer.is_valid(raise_exception=True)
+        book.authors.set(authors)
+
+        return Response(serializer.data)
