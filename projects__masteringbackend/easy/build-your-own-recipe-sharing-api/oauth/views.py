6058e245561faf39127a4a724aa22f0a05fd0d1c
Angel Vasquez <angel.vasquez.unam@gmail.com>
2025-01-25 11:34:10 -0600

    Tastybits: RegisterView returns token

--- projects__masteringbackend/easy/build-your-own-recipe-sharing-api/oauth/views.py
+++ projects__masteringbackend/easy/build-your-own-recipe-sharing-api/oauth/views.py
@@ -1,6 +1,8 @@
+from rest_framework import generics, status
+from rest_framework.response import Response
 from rest_framework_simplejwt.views import TokenObtainPairView as BaseTokenObtainPairView
+from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework.permissions import AllowAny
-from rest_framework import generics
 from django.contrib.auth import get_user_model
 from .serializers import TokenObtainPairSerializer, RegisterSerializer
 
@@ -17,3 +19,18 @@ class RegisterView(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = RegisterSerializer
     permission_classes = (AllowAny,)
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        try:
+            user = serializer.save()
+        except Exception as e:
+            return Response({"detail": str(e)}, status=status.HTTP_400_BAD_REQUEST)
+        refresh = RefreshToken.for_user(user)
+        response = {
+            'user': serializer.data,
+            'refresh': str(refresh),
+            'access': str(refresh.access_token),
+        }
+        return Response(response, status=status.HTTP_201_CREATED)
