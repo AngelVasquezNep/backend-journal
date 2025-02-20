61a4ac4a4d22e006f4061fae7ea92a56b452896a
Angel Vasquez <angel.vasquez.unam@gmail.com>
2025-02-19 20:59:14 -0600

    Tastybits: Create EmailSender

---
+++ projects__masteringbackend/easy/build-your-own-recipe-sharing-api/core/services/email/__init__.py
@@ -0,0 +1,29 @@
+import logging
+from typing import Optional
+from enum import Enum
+
+logger = logging.getLogger(__name__)
+
+
+class Senders(Enum):
+    GMAIL = "GMAIL"
+
+
+class EmailSender:
+    def __init__(self, provider: Senders):
+        match provider:
+            case Senders.GMAIL:
+                from .gmail import GmailService
+                self.sender = GmailService()
+            case _:
+                raise NotImplementedError(
+                    f"Provider {provider} not implemented")
+
+    def send_message(
+        self,
+        To,
+        Subject: Optional[str] = None,
+        Content: Optional[str] = None,
+        From: Optional[str] = None,
+    ):
+        return self.sender.send_message(To, Subject, Content, From)
