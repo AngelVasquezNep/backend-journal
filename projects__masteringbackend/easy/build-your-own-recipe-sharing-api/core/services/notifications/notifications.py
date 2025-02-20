50be4086fdb89cf72efbd652b5f2be2f559dccd3
Angel Vasquez <angel.vasquez.unam@gmail.com>
2025-02-19 21:00:35 -0600

    Tastybits: Create EmailService using EmailSender with GMAIL

---
+++ projects__masteringbackend/easy/build-your-own-recipe-sharing-api/core/services/notifications/notifications.py
@@ -0,0 +1,28 @@
+import logging
+from typing import Optional
+from core.services.email import EmailSender, Senders
+
+logger = logging.getLogger(__name__)
+
+
+class NotificationsService:
+    @staticmethod
+    def send_one(
+            recipient,
+            subject: Optional[str] = None,
+            body: Optional[str] = None,):
+        raise NotImplementedError(
+            "send_notification method is not implemented")
+
+    @staticmethod
+    def send_many(recipients, body):
+        for recipient in recipients:
+            NotificationsService.send_one(recipient, body)
+
+
+class EmailService(NotificationsService):
+    @staticmethod
+    def send_one(recipient,
+                 subject: Optional[str] = None,
+                 body: Optional[str] = None,):
+        EmailSender(Senders.GMAIL).send_message(recipient, subject, body)
